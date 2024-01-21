5e32a1d3f9f1570ff6cd707a2223831de4667413
Jonathan da Silva <jonathansilva15sp@gmail.com>
2024-01-21 15:52:38 -0300

    Requests controller

--- BE/LH-PEITAS/src/main/java/br/com/lhp/controller/ImagemController.java
+++ BE/LH-PEITAS/src/main/java/br/com/lhp/controller/ImagemController.java
@@ -1,6 +1,7 @@
 package br.com.lhp.controller;
 
 
+import java.io.BufferedReader;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -8,6 +9,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 import br.com.lhp.model.Imagem;
 import br.com.lhp.service.ImagemService;
@@ -19,18 +23,37 @@ public class ImagemController extends HttpServlet{
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String imagem = request.getParameter("link");
-		String pasta = request.getParameter("pasta");
-		String cod = request.getParameter("codigo");
-		Imagem img = new Imagem(pasta, imagem);
-		try {
-			service.cadastrarImagem(img, Integer.parseInt(cod));
-			response.setStatus(200);
-			response.getWriter().print("Sucesso");
-		}catch(RuntimeException e) {
-			response.setStatus(500);
-			response.getWriter().print("Erro "+ e.getMessage());
-
+		if(request.getContentType().equals("application/json")) {
+			BufferedReader  data =  request.getReader();
+			StringBuilder obj = new StringBuilder();
+			String linha;
+			
+			while((linha = data.readLine()) != null ){
+				obj.append(linha);
+				System.out.println(linha);
+			}
+			
+			JsonObject jsonTxt = new Gson().fromJson(obj.toString(), JsonObject.class);
+			
+			try {
+				Imagem img = new Imagem(jsonTxt.get("pasta").getAsString(), jsonTxt.get("link").getAsString());
+				try {
+					service.cadastrarImagem(img, Integer.parseInt(jsonTxt.get("cod").getAsString()));
+					response.setStatus(200);
+					response.getWriter().print("Sucesso");
+				}catch(RuntimeException e) {
+					response.setStatus(500);
+					response.getWriter().print("Erro "+ e.getMessage());
+					
+				}
+			}catch(NullPointerException e) {
+				response.setStatus(404);
+//				response.getWriter().print("");
+			}
+			
+		}else {
+			response.setStatus(406);
+			response.getWriter().print("Content-type Invalido");
 		}
 		response.getStatus();
 	}
