85c66f6e2e990d16898c5cdc2c1b6c149f01ff5a
Jonathan da Silva <jonathansilva15sp@gmail.com>
2024-02-18 18:34:35 -0300

    Organizando js

--- BE/LH-PEITAS/src/main/WebContent/view/script.js
+++ BE/LH-PEITAS/src/main/WebContent/view/script.js
@@ -1,5 +1,10 @@
 const arrowDownIcon = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAJxJREFUSEvtk9ENgCAMRI9NHE0nUSfR0RxFm0jSIO1VE34MfB/36AMSGq/UuB8dQA3/X9EA4KAenICnaLvLVwJwcxZANo138QLAgtCcBZivcinOqwbR5ZLbAUzltJ4iDxIqFxh7pjWIXHzWZ548T8IAkish2kJViw5EABaElkcU6cPoSULlbwF5ErmDx2ux/kpU0efP3AFUXVdEFZ3dzBsZFLiuBQAAAABJRU5ErkJggg=="
 const arrowUpIcon = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAJZJREFUSEvtk7ENgDAMBD+bMBpMAkwCo8EmYMmRTJTETxFRkDQ07zvzgYDGJzTmowvchv9X0QbgALC63WjgTUUCH3VuYSWswMLj8pSEEeTgtMQTpPBd70C2pyQ1QQ4+KXW+n5SkJKjB4+aUpCSww1JL3Dz9Om1OMpJ9nFpFMjxU4PZNzhxcAt4ls/9TMdcFboW9ou8rugDk6xsZXz5RRwAAAABJRU5ErkJggg==";
+const allSkeleton = document.querySelectorAll('.skeleton');
+const navSearch = document.querySelector('#nav-search');
+const filtro = document.getElementById('filtro-icon');
+const filtroCor = document.querySelector('.filtro-listCor_content');
+const filtroLiga = document.querySelector('.filtro-listLiga_content');
 class uniformes{
     constructor(equipe, link, temporada, uniforme){
         this.equipe = equipe;
@@ -19,74 +24,9 @@ class uniformes{
 //          body: JSON.stringify({time:'',cor:'',liga:''})
 //     };
 
-function getService(URL, seletor){  
-    let options = {
-            mode: 'cors',
-            method: 'GET',
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json;charset=utf-8"
-                }
-            };
-
-    fetch(`${URL}`,options)
-        .then((resp) => resp.json())
-        .then((data) => criarList(data,seletor))
-        .catch(e =>
-            console.log(e.message))
-}
-
-function request(URL){
-    const options = {
-        mode: 'cors',
-        method: 'GET',
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/json;charset=utf-8"
-            }
-        };
-    fetch(`${URL}`,options)
-    .then(resp =>resp.json())
-    .then(data => {
-        criarCard(data)
-    })
-    .catch(e =>
-        console.log(e.message))
-}
-
-function nomeCores(item){
-    let li = document.getElementById(`${item}`);
-    li.style.backgroundColor = li.textContent;
-    li.addEventListener('click', ()=>{
-        let cards = document.querySelectorAll(".card");
-        cards.forEach(card =>{
-            card.remove();
-        })
-        request(`http://localhost:8088/LH-PEITAS/base?time&cor=${item}&liga`);
-    })
-}
-
-function nomeLigas(item){
-    let li = document.getElementById(`${item.replace(" ","")}`);
-    
-    li.addEventListener('click', ()=>{
-        let cards = document.querySelectorAll(".card");
-        cards.forEach(card =>{
-            card.remove();
-        })
-        request(`http://localhost:8088/LH-PEITAS/base?time&cor&liga=${item}`);
-    })
-
-}
-
-//Inicializar 
-request('http://localhost:8088/LH-PEITAS/base?time&cor&liga');
-getService("http://localhost:8088/LH-PEITAS/listUniformes/cores", "#filtro-listCor-ul");
-getService("http://localhost:8088/LH-PEITAS/listarTimes/ligas", "#filtro-listLiga-ul");
-
-// Criar
+// Create Functions
 function criarList(list, seletor){
-    const filtroUl = document.querySelector(`${seletor}`);
+    let filtroUl = document.querySelector(`${seletor}`);
     list.forEach(
         item =>{
             let li = document.createElement('li')
@@ -105,10 +45,9 @@ function criarList(list, seletor){
                 nomeLigas(item);
             }
         }
-        )
-        console.log("rodou function");
-    }
-
+    )
+    console.log("rodou function");
+}
 
 function criarCard(array){
     array.forEach(element => {
@@ -133,92 +72,135 @@ function criarCard(array){
         card.appendChild(corDiv);
         card.setAttribute('class','card skeleton');
         idList.appendChild(card);
+        let allSkeleton = document.querySelectorAll('.skeleton');
         function skeleton(){
-            const allSkeleton = document.querySelectorAll('.skeleton');
             allSkeleton.forEach(item=>{
                 item.classList.remove('skeleton')
             })}
         skeleton();
     });
 }
-// End Criar
 
+function nomeCores(item){
+    let li = document.getElementById(`${item}`);
+    li.style.backgroundColor = li.textContent;
+    li.addEventListener('click', ()=>{
+        let cards = document.querySelectorAll(".card");
+        cards.forEach(card =>{
+            card.remove();
+        })
+        request(`http://localhost:8088/LH-PEITAS/base?time&cor=${item}&liga`);
+    })
+}
+
+function nomeLigas(item){
+    let li = document.getElementById(`${item.replace(" ","")}`);
+    li.addEventListener('click', ()=>{
+        let cards = document.querySelectorAll(".card");
+        cards.forEach(card =>{
+            card.remove();
+        })
+        request(`http://localhost:8088/LH-PEITAS/base?time&cor&liga=${item}`);
+    })
+
+}
+// Requests functions
+function getList(URL, seletor){  
+    let options = {
+            mode: 'cors',
+            method: 'GET',
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json;charset=utf-8"
+                }
+            };
+
+    fetch(`${URL}`,options)
+        .then((resp) => resp.json())
+        .then((data) => criarList(data,seletor))
+        .catch(e => console.log(e.message))
+}
+
+function request(URL){
+    const options = {
+        mode: 'cors',
+        method: 'GET',
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json;charset=utf-8"
+            }
+        };
+    fetch(`${URL}`,options)
+    .then(resp =>resp.json())
+    .then(data => {
+        criarCard(data)
+    })
+    .catch(e =>
+        console.log(e.message))
+} // END Requests functions
+// Hidden list
+function hidden(display, element){
+    if(element.getAttribute('hidden') == "false"){
+        element.setAttribute('style','display:none')
+        element.removeAttribute('hidden');
+        element.setAttribute('hidden','true')
+    }else{
+        element.setAttribute('style',`display:${display}`)
+        element.removeAttribute('hidden');
+        element.setAttribute('hidden','false')
+    }
+}
+
+function changeArrow(boolean, icon){
+    if(boolean == "false"){
+        icon.removeAttribute('src');
+        icon.setAttribute('src', arrowUpIcon)
+    }else{
+        icon.removeAttribute('src');
+        icon.setAttribute('src', arrowDownIcon)
+    }
+}// END Hidden list
+// END Create
+
+//Initialize 
+request('http://localhost:8088/LH-PEITAS/base?time&cor&liga');
+getList("http://localhost:8088/LH-PEITAS/listUniformes/cores", "#filtro-listCor-ul");
+getList("http://localhost:8088/LH-PEITAS/listarTimes/ligas", "#filtro-listLiga-ul");
+// END Initialize
+
+// Events
 // Skeleton
-const allSkeleton = document.querySelectorAll('.skeleton');
-	window.addEventListener('load', ()=>{
-		allSkeleton.forEach(item=>{
-			item.classList.remove('skeleton')
-		})
+window.addEventListener('load', ()=>{
+	allSkeleton.forEach(item=>{
+		item.classList.remove('skeleton')
 	})
-// End Skeleton
+})// End Skeleton
 
-// Esconder list
-const filtro = document.getElementById('filtro-icon');
 filtro.addEventListener("click", ()=>{
     let list = document.querySelector('.filtro-list')
-    if(list.getAttribute('hidden') == "false"){
-        list.setAttribute('style','display:none')
-        list.removeAttribute('hidden');
-        list.setAttribute('hidden','true')
-    }else{
-        list.setAttribute('style','display:flex')
-        list.removeAttribute('hidden');
-        list.setAttribute('hidden','false')
-
-    }
+    hidden("flex",list);
 })
 
-const filtroCor = document.querySelector('.filtro-listCor_content');
 filtroCor.addEventListener('click', ()=>{
     let list = document.querySelector('#filtro-listCor-ul');
     let img = document.querySelector("#filtro-listCor-Icon");
-    if(list.getAttribute('hidden') == "false"){
-        img.removeAttribute('src');
-        img.setAttribute('src', arrowDownIcon)
-        list.setAttribute('style','display:none');
-        list.removeAttribute('hidden');
-        list.setAttribute('hidden','true')
-    }else{
-        img.removeAttribute('src');
-        img.setAttribute('src', arrowUpIcon)
-        list.setAttribute('style','display:flex');
-        list.removeAttribute('hidden');
-        list.setAttribute('hidden','false');
-        
-    }
+    hidden("flex",list);
+    changeArrow(list.getAttribute('hidden'),img)
 });
 
-const filtroLiga = document.querySelector('.filtro-listLiga_content');
 filtroLiga.addEventListener('click', ()=>{
     let list = document.querySelector('#filtro-listLiga-ul')
     let img = document.querySelector("#filtro-listLiga-Icon");
-    if(list.getAttribute('hidden') == "false"){
-        img.removeAttribute('src');
-        img.setAttribute('src', arrowDownIcon)
-        list.setAttribute('style','display:none')
-        list.removeAttribute('hidden');
-        list.setAttribute('hidden','true')
-    }else{
-        img.removeAttribute('src');
-        img.setAttribute('src', arrowUpIcon)
-        list.setAttribute('style','display:block')
-        list.removeAttribute('hidden');
-        list.setAttribute('hidden','false')
-        
-    }
+    hidden("block",list);
+    changeArrow(list.getAttribute('hidden'),img);
 });
-//
 
-// Buscar por Time
-const navSearch = document.querySelector('#nav-search');
 navSearch.addEventListener('input' , (e)=>{
     let text = e.target.value;
-
     const cards = document.querySelectorAll(".card");
     cards.forEach(item =>{
         item.remove();
     })
-
     request(`http://localhost:8088/LH-PEITAS/base?time=${text.toLowerCase()}&cor&liga`);
 })
-//
+// END Events
